5c81b0f12b7c1120dd1529ec532050190c332f56
Cesar Morales <cesarlenin2@live.com>
2020-05-18 17:10:06 -0700

    seed created

--- migrations/001.do.create_my_discipline_habit.sql
+++ migrations/001.do.create_my_discipline_habit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE my_discipline_habit (
   id SERIAL PRIMARY KEY,
   habit_name TEXT NOT NULL,
-  goal TEXT NOT NULL,
+  goal INTEGER NOT NULL,
   description TEXT NOT NULL,
   date_created TIMESTAMPTZ DEFAULT now() NOT NULL
 );
