205e97c92b75f05bf1de4935674cb1a56a617681
Cesar Morales <cesarlenin2@live.com>
2020-05-20 14:01:45 -0700

    action post works

--- src/actions/actions-router.js
+++ src/actions/actions-router.js
@@ -19,16 +19,22 @@ actionsRouter
 
   .post(bodyParser, (req, res, next) => {
     const { 
-      date_created
+      date_created,
+      habit_id
     } = req.body;
     
     const newActions = { 
-      date_created
+      date_created,
+      habit_id,
+      user_id: req.user.id
     };
-
-    if (!date_created || !Number.isInteger(Number(date_created))) {
+    if (!date_created || new Date(date_created)=== 'Invalid Date') {
       logger.error('date needs to be a date');
-      return res.status(400).send('goal needs to be a date');
+      return res.status(400).send('date needs to be a date');
+    } 
+    if (!habit_id || !Number.isInteger(Number(habit_id))) {
+      logger.error('habit id needs to be a number');
+      return res.status(400).send('habit id needs to be a number');
     } 
 
     ActionsService.insertAction(req.app.get('db'),newActions)
